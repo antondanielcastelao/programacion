24f4bda36c8165d9dfc5271b193bbd97872741f4
Anton Davila <rdavilacarro@danielcastelao.org>
2024-10-21 14:28:22 +0200

    implementamos cuenta atras con bucle for

---
+++ cuenta atras for/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Cuenta atrás!!");
+
+        for(int c = 10; c > 0; c-=1) {
+            System.out.println(c);
+        }
+
+        System.out.println("Tiempo!");
+    }
+}

30f1f94461315c169ec029cc24e1ecaca59bfbec
Anton Davila <rdavilacarro@danielcastelao.org>
2024-11-21 12:41:33 +0100

    ejercicio 1 resuelto

---
+++ boletin7/src/ejercicio1.java
@@ -0,0 +1,9 @@
+public class ejercicio1 {
+    public static void main(String[] args) {
+        int[] numeros = {1, 15, 37, 12, 23, 44};
+
+        for (int i = numeros.length-1; i >= 0; i--) {
+            System.out.println(numeros[i]);
+        }
+    }
+}

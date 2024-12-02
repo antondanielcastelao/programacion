6e515db06bfe2a29c1dc16efdd2e7cb9eefa2dcd
Anton Davila <rdavilacarro@danielcastelao.org>
2024-12-02 13:54:38 +0100

    resuelto, faltan algunos dias pero ya va guay

--- teatro/src/Main.java
+++ teatro/src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // definimos el tamaño del teatro
+        boolean[][] asientos = new boolean[4][4];
+
+        asientos[0][0] = true;
+        asientos[0][1] = true;
+        asientos[0][2] = true;
+        asientos[0][3] = false;
+
+        asientos[1][0] = true;
+        asientos[1][1] = true;
+        asientos[1][2] = true;
+        asientos[1][3] = false;
+
+        asientos[1][0] = true;
+        asientos[1][1] = true;
+        asientos[1][2] = true;
+        asientos[1][3] = false;
+
+        asientos[1][0] = true;
+        asientos[1][1] = true;
+        asientos[1][2] = true;
+        asientos[1][3] = false;
     }
 }

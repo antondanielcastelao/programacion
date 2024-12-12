fb8855038addb261b66fa182a7f94fe22640d9a0
Anton Davila <rdavilacarro@danielcastelao.org>
2024-12-12 13:33:10 +0100

    creamos la funcion para mostrar las palabras

---
+++ laIsla/src/Main.java
@@ -0,0 +1,32 @@
+/**
+ * Programa simulador del famoso juego de fumar porros "La Isla"
+ * @author Conguchu y Saul
+ * @version 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        String [] palabras = {};
+        String [] jugadores = {};
+
+    }
+
+    /**
+     * Función = Muestra las palabras del array unidimensional "palabras", declarado en la main.
+     * @param palabras
+     * @return
+     */
+    public static String[] mostrarPalabras (String [] palabras){
+        for (int i = 0; i < palabras.length; i++){
+            System.out.println(palabras[i]);
+        }
+        return palabras;
+    }
+
+    public static String [] añadirPalabras (String [] palabras){
+
+
+
+    }
+
+}

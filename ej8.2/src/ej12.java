ca10154eb099297187e2ceb3eddbf4452edb735d
Anton Davila <rdavilacarro@danielcastelao.org>
2025-01-09 12:40:39 +0100

    ej 1.2 completado

---
+++ ej8.2/src/ej12.java
@@ -0,0 +1,8 @@
+public class ej12 {
+    public static void main(String[] args) {
+
+        String texto = "Hola mundo";
+
+        System.out.println(texto.split("")[texto.length()-3] + texto.split("")[texto.length()-2] + texto.split("")[texto.length()-1]);
+    }
+}

550a120d1e18b6c487f0ad683fd56f71de192304
Anton Davila <rdavilacarro@danielcastelao.org>
2025-01-09 13:19:01 +0100

    ejercicio 2 resuelto

--- ej8.2/src/ej2.java
+++ ej8.2/src/ej2.java
@@ -4,12 +4,15 @@ public class ej2 {
         // ej 1
         System.out.println(comas(texto));
         // ej 2
-
-
+        System.out.println(espazos(texto));
+        // ej 3
+        System.out.println(dixitos(texto));
+        // ej 4
+        System.out.println(cada3dixitos(texto));
     }
 
     /**
-     * ejercicio 1
+     * ejercicio 2a
      * @param texto
      * @return
      */
@@ -25,4 +28,63 @@ public class ej2 {
         }
         return result;
     }
+
+    /**
+     * Ejercicio 2c
+     * @param texto
+     * @return
+     */
+    public static String dixitos (String texto) {
+        String[] textoSeparado = texto.split("");
+        String result = "";
+        for (int i = 0; i< textoSeparado.length; i++) {
+            String letra = textoSeparado[i];
+            if (textoSeparado[i].matches("-?\\d+(\\.\\d+)?")) {
+                letra = "X";
+            }
+            result += letra;
+        }
+        return result;
+    }
+    /**
+     * Ejercicio 2b
+     * @param texto
+     * @return
+     */
+    public static String espazos (String texto) {
+        String[] textoSeparado = texto.split("");
+        String result = "";
+        for (int i = 0; i< textoSeparado.length; i++) {
+            String letra = textoSeparado[i];
+            if (textoSeparado[i].equals(" ")) {
+                letra = "\\_";
+            }
+            result += letra;
+        }
+        return result;
+    }
+
+    /**
+     * Ejercicio 2d
+     * @param texto
+     * @return
+     */
+    public static String cada3dixitos (String texto) {
+        String[] textoSeparado = texto.split("");
+        String result = "";
+
+        for (int i = 0; i< textoSeparado.length; i+=3) {
+            String letra = textoSeparado[i];
+            if (i != textoSeparado.length-3) {
+                letra = textoSeparado[i] + textoSeparado[i + 1] + textoSeparado[i + 2] + ".";
+            } else {
+                letra = textoSeparado[i] + textoSeparado[i + 1] + textoSeparado[i + 2];
+
+            }
+
+            result += letra;
+        }
+        return result;
+    }
+
 }
